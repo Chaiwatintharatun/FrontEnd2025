5a7f1be363ae22c973651cd5f6bb89d54b653c95
Chaiwatintharatun <119995243+Chaiwatintharatun@users.noreply.github.com>
2025-03-12 18:16:59 +0700

    Update index.js

--- frontend/frontend/index.js
+++ frontend/frontend/index.js
@@ -12,7 +12,7 @@ const agent = new https.Agent({ rejectUnauthorized: false });
 
 // const base_url = "http://localhost:3000";
 // const base_url = "http://10.104.7.149";
-const base_url = "http://node77040-env-0528958.proen.app.ruk-com.cloud:11620";
+const base_url = "http://node77040-env-0528958.proen.app.ruk-com.cloud";
 
 app.set('views', path.join(__dirname, "/public/views"));
 app.set('view engine', 'ejs');
